05ff12847f2d222bd64b3518d9f25cb4d0781047
Rüveyda Dal <71987230+ruv19@users.noreply.github.com>
2021-04-26 16:33:12 +0300

    Add files via upload

---
+++ hipotenus bulma.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+int main (void)
+{
+	float kenar1,kenar2,kenar3,sonuc;
+	printf("\n HÝPOTENUS HESABÝ.");
+	printf("\n kenar1=");
+	scanf("%f",&kenar1);
+	printf("\n kenar2=");
+	scanf("%f",&kenar2);
+	printf("sonuc=(kenar1*kenar1)+(kenar2*kenar2)=%f",(kenar1*kenar1)+(kenar2*kenar2));
+	sonuc=sqrt(sonuc);
+	printf("\n sonuc=%lf",sonuc);
+	
+	
+	system("pause");
+	return 0;
+}
